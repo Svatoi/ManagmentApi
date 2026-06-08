bbfa3cd948dea324bd77a8ba8fefe871614b5e63
Svatoi <privitvlad00@gmail.com>
2026-06-08 15:39:45 +0200

    Pagination, filtering and product search

--- app/api/item.py
+++ app/api/item.py
@@ -1,5 +1,6 @@
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List, Optional
+from fastapi import APIRouter, Depends, status, Query
+
 from sqlalchemy.orm import Session
 
 from app.database import get_db
@@ -28,6 +29,29 @@ def create_item(item_in: ItemCreate, db: Session = Depends(get_db), current_admi
     return db_item
 
 @router.get("/", response_model=List[ItemOut])
-def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    items = db.query(Item).offset(skip).limit(limit).all()
+def read_items(
+    skip: int = 0, 
+    limit: int = 10, 
+    search: Optional[str] = Query(None, description="Search by product name"),
+    item_type: Optional[str] = Query(None, description="Filter by type/category of product"),
+    min_price: Optional[float] = Query(None, description="Minimum price"),
+    max_price: Optional[float] = Query(None, description="Maximum price"),
+    db: Session = Depends(get_db)
+    ):
+
+    query = db.query(Item)
+
+    if search:
+        query = query.filter(Item.item_name.ilike(f"%{search}%"))
+
+    elif item_type:
+        query = query.filter(Item.type == item_type)
+
+    elif min_price is not None:
+        query = query.filter(Item.price >= min_price) 
+
+    elif max_price is not None:
+        query = query.filter(Item.price <= max_price)
+
+    items = query.offset(skip).limit(limit).all()
     return items
